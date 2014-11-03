4eb3af15e7afa4005b66a9783e185050420b36db
corinne krych <corinnekrych@gmail.com>
2014-11-03 16:36:25 +0100

    clear appDelegate and plist

--- Shoot/Shoot/AppDelegate.swift
+++ Shoot/Shoot/AppDelegate.swift
@@ -62,10 +62,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String, annotation: AnyObject?) -> Bool {
-        let notification = NSNotification(name: AGAppLaunchedWithURLNotification, object:nil, userInfo:[UIApplicationLaunchOptionsURLKey:url])
-        NSNotificationCenter.defaultCenter().postNotification(notification)
-        return true
-    }
+
 }
 
